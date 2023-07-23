db712e910df570c5a613ba9b9b6c51b08b1008ea
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2023-07-23 14:47:41 +0530

    Remove typescript from this project

--- react-basics/react-query-demo/src/index.js
+++ react-basics/react-query-demo/src/index.js
@@ -2,9 +2,15 @@ import ReactDom from "react-dom/client";
 import * as React from "react";
 import App from "./App";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
